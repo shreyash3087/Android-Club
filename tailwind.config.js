c535d61e7d15935a2e425ffdaf537d385631b457
shreyash3087 <shreyash3087@gmail.com>
2024-04-23 21:06:03 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {boxShadow: {
+      'custom': '0 0 20px 5px #6E9DD9',
+    }},
+  },
+  plugins: [],
+}
+
